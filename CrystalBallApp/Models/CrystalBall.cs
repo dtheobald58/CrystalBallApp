7857f2a9deadf98d2baebd46c6e9e90970cbb254
dtheobald58 <dtheobald58@yahoo.com>
2016-10-26 09:26:49 -0400

    end

---
+++ CrystalBallApp/Models/CrystalBall.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrystalBallApp.Models
+{
+    public class CrystalBall
+    {
+        public string getResponse()
+        {
+            string[] responses = { "Response 1", "Response 2", "Response 3" };
+            Random randomNum = new Random();
+            var num = randomNum.Next(0, responses.Length);
+            return responses[num];
+        }
+
+    }
+}
